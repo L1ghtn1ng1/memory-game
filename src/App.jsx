6043e56a3940ddf17ada3924d95a5570b8e7bce5
L1ghtn1ng1 <77595914+L1ghtn1ng1@users.noreply.github.com>
2025-02-03 20:34:45 +1300

    implemented shuffle of pictures and score updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,39 @@
-import { useState } from 'react'
-import CardList from './components/CardList'
-import './App.css'
+import { useState } from "react";
+import CardList from "./components/CardList";
+import "./App.css";
 
 function App() {
-  const [score, setScore] = useState(0)
-  const [currentCards, setCurrentCards] = useState([])
+  const [score, setScore] = useState(0);
+  const [currentCards, setCurrentCards] = useState([]);
+  const [highScore, setHighScore] = useState(0);
+
+  const saveScore = (id) => {
+    if (currentCards.includes(id)) {
+      if (score > highScore) {
+        setHighScore(score);
+      }
+      setScore(0);
+      setCurrentCards([]);
+    } else {
+      setScore(score + 1);
+      setCurrentCards([...currentCards, id]);
+    }
+  };
+  console.log(score);
+
   return (
     <>
-      <CardList />
+      <div className="header">
+        <h1 className="title">Memory Card Game</h1>
+        <div className="scores">
+          <h2 className="score">Score: {score}</h2>
+          <h2 className="high-score">High Score: {highScore}</h2>
+        </div>
+      </div>
+
+      <CardList saveScore={saveScore} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
